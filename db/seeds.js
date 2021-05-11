3e9b0ac66d9bad8a093afb5f240601d3f9356d31
chris westerduin <chriswesterduin@outlook.com>
2021-05-11 23:29:45 +0100

    update seeds

--- db/seeds.js
+++ db/seeds.js
@@ -5,5 +5,6 @@ const db = connect("mongodb://localhost:27017/mydb")
 db.app_url.drop()
 
 db.app_url.insertMany([
-    { url: "www.google.com" },
+    { url: "http://twitter.com/" },
+    { url: "http://google.com/" },
 ])
