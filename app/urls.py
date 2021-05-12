0218fc0b3372bc4ef0ed6ef5aee2199379268ca6
chris westerduin <chriswesterduin@outlook.com>
2021-05-12 10:44:33 +0100

    add form for shortening url

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.urls, name='urls-all'),
-    path('<str:id>/', views.redirector, name='urls-redirect')
+    path('<str:id>/', views.redirector, name='urls-redirect'),
+    path('url/<str:id>/', views.show, name='urls-show')
     ]
--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=)#v8l^f(dyh8*c9hyyixq-5ip+^j*wy1h^-%ubdu(f)$18vwb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
